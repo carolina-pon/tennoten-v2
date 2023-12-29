6182c47017232fcc9e6fbf473a9f8e3d288b56c5
lina <mad.20th@gmail.com>
2023-12-29 15:03:48 +0900

    Add: 展示時期設定ののCRUD追加

---
+++ app/controllers/api/exhibition_periods_controller.rb
@@ -0,0 +1,26 @@
+class Api::ExhibitionPeriodsController < ApplicationController
+  def index
+    @exhibition_periods = ExhibitionPeriod.all
+    render json: @exhibition_periods
+  end
+
+  def create
+    @exhibition_period = ExhibitionPeriod.create(exhibition_period_params)
+    render json: @exhibition_period
+  end
+
+  def update
+    @exhibition_period = ExhibitionPeriod.find(params[:id])
+    @exhibition_period.update_attributes(exhibition_period_params)
+    render json: @exhibition_period
+  end
+
+  def destroy
+    @exhibition_period = ExhibitionPeriod.find(params[:id])
+    if @exhibition_period.destroy
+      head :no_content, status: :ok
+    else
+      render json: @exhibition_period.errors, status: :unprocessable_entity
+    end
+  end
+end
